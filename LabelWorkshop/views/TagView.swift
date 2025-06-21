e7663763931a4a7846c7b92b4e891188e1db2cfe
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2025-06-20 22:44:19 -0400

    feat(ui): show preview of tag

--- LabelWorkshop/views/TagView.swift
+++ LabelWorkshop/views/TagView.swift
@@ -1,6 +1,24 @@
 import SwiftUI
 
-
+struct TagPreView: View {
+    @Binding public var name: String
+    @Binding public var color: Color
+    @Binding public var borderColor: Color
+    @Binding public var textColor: Color
+    
+    var body: some View {
+        Text(name)
+            .foregroundStyle(textColor)
+            .font(.body)
+            .padding(8)
+            .background(color)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(borderColor, lineWidth: 8)
+            )
+            .cornerRadius(8)
+    }
+}
 
 struct TagView: View {
     let tag: Tag
