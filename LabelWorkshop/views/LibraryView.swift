197ac066a861dc443e26968421811461249894b1
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2026-07-10 16:25:23 -0400

    refactor: move tag filtering into view

--- LabelWorkshop/views/LibraryView.swift
+++ LabelWorkshop/views/LibraryView.swift
@@ -77,47 +77,7 @@ struct LibraryView: View {
                 .tint(tagFilters.isEmpty ? .primary : .blue)
                 .popoverTip(tagFilterTip, arrowEdge: .bottom)
                 .sheet(isPresented: $showTagfilter) {
-                    NavigationView {
-                        ScrollView {
-                            VStack {
-                                ForEach(tags, id: \.id) { tag in
-                                    HStack {
-                                        Image(systemName: tagFilters.contains(where: { filterTag in return filterTag.id == tag.id}) ? "checkmark.circle" : "circle")
-                                        Button(action: {
-                                            if tagFilters.contains(where: { filterTag in
-                                                return filterTag.id == tag.id
-                                            }) {
-                                                tagFilters.removeAll(where: { filterTag in
-                                                    return filterTag.id == tag.id
-                                                })
-                                            } else {
-                                                tagFilters.append(tag)
-                                            }
-                                        }) {
-                                            TagPreView(name: .constant(tag.name), colors: .constant(tag.colors), fullWidth: true)
-                                        }
-                                    }
-                                }
-                            }
-                            .padding(16)
-                        }
-                        .toolbar {
-                            ToolbarItem(placement: .navigationBarLeading){
-                                Button(action: {
-                                    showTagfilter = false
-                                }) {
-                                    Image(systemName: "chevron.backward")
-                                }
-                            }
-                            ToolbarItem(placement: .bottomBar){
-                                Button(action: {
-                                    tagFilters = []
-                                }) {
-                                    Text("Deselect All")
-                                }
-                            }
-                        }
-                    }
+                    TagFilterView(tags: $tags, tagFilters: $tagFilters)
                     .task {
                         self.tags = Tag.fetchAll(library: self.library)
                     }
