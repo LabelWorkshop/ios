802c77c9055b508e2d6699661bb6f65a2b56501e
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2026-07-13 03:14:33 -0400

    feat: database migration up to 103

--- LabelWorkshop/classes/Field.swift
+++ LabelWorkshop/classes/Field.swift
@@ -74,3 +74,33 @@ class Field: Identifiable, Hashable {
         self.type = entry.library.fieldTypes.first(where: {$0.id == id})
     }
 }
+
+let LEGACY_FIELD_MAP = [
+    0: ["type": "text", "name": "Title", "is_multiline": false],
+    1: ["type": "text", "name": "Author", "is_multiline": false],
+    2: ["type": "text", "name": "Artist", "is_multiline": false],
+    3: ["type": "text", "name": "URL", "is_multiline": false],
+    4: ["type": "text", "name": "Description", "is_multiline": true],
+    5: ["type": "text", "name": "Notes", "is_multiline": true],
+    9: ["type": "text", "name": "Collation", "is_multiline": false],
+    10: ["type": "datetime", "name": "Date", "is_multiline": false],
+    11: ["type": "datetime", "name": "Date Created"],
+    12: ["type": "datetime", "name": "Date Modified"],
+    13: ["type": "datetime", "name": "Date Taken"],
+    14: ["type": "datetime", "name": "Date Published"],
+    17: ["type": "text", "name": "Book", "is_multiline": false],
+    18: ["type": "text", "name": "Comic", "is_multiline": false],
+    19: ["type": "text", "name": "Series", "is_multiline": false],
+    20: ["type": "text", "name": "Manga", "is_multiline": false],
+    21: ["type": "text", "name": "Source", "is_multiline": false],
+    22: ["type": "datetime", "name": "Date Uploaded"],
+    23: ["type": "datetime", "name": "Date Released"],
+    24: ["type": "text", "name": "Volume", "is_multiline": false],
+    25: ["type": "text", "name": "Anthology", "is_multiline": false],
+    26: ["type": "text", "name": "Magazine", "is_multiline": false],
+    27: ["type": "text", "name": "Publisher", "is_multiline": false],
+    28: ["type": "text", "name": "Guest Artist", "is_multiline": false],
+    29: ["type": "text", "name": "Composer", "is_multiline": false],
+    30: ["type": "text", "name": "Comments", "is_multiline": true],
+]
+
