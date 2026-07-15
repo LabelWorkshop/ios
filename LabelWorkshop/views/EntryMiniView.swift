50fcb7fa07337442557f88ca6143631e283c47eb
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2026-07-14 22:54:46 -0400

    feat(ui): updated entry look in libraries

--- LabelWorkshop/views/EntryMiniView.swift
+++ LabelWorkshop/views/EntryMiniView.swift
@@ -11,14 +11,19 @@ struct EntryMiniView: View {
         NavigationLink(destination: EntryView(entry: entry).id(entry.id)){
             VStack(spacing: 0) {
                 EntryPreView(entry: entry, square: true)
+                .background(Color(UIColor.secondarySystemBackground))
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color(UIColor.secondarySystemBackground), lineWidth: 1)
+                )
                 Text(entry.path)
                     .font(.caption)
                     .lineLimit(1)
-                    .padding(8)
+                    .padding(EdgeInsets(top: 8, leading: 2, bottom: 4, trailing: 2))
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .foregroundStyle(Color(UIColor.label))
-            }.background(Color(UIColor.secondarySystemFill))
-            .clipShape(RoundedRectangle(cornerRadius: 24))
+            }
         }
     }
 }
