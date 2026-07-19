46520f47251c67d9f8a3fbd2de22d5197d49460a
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2026-07-19 19:39:47 -0400

    refactor(ui): use list in tag editor parents

--- LabelWorkshop/views/TagEditor/Parents.swift
+++ LabelWorkshop/views/TagEditor/Parents.swift
@@ -8,70 +8,68 @@ struct TagEditorParents: View {
     var tags: [Tag]
     
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach($parentTags){ $tag in
-                    HStack {
-                        Button(action: {
-                            if disambiguationId != tag.id {
-                                disambiguationId = tag.id
-                            } else {
-                                disambiguationId = nil
-                            }
-                        }, label: {
-                            HStack {
-                                Image(systemName: disambiguationId == tag.id ? "checkmark.circle" : "circle").font(.title)
-                            }
-                        })
-                        TagView(tag: tag, fullWidth: true)
+        List {
+            ForEach($parentTags){ $tag in
+                HStack {
+                    Button(action: {
+                        if disambiguationId != tag.id {
+                            disambiguationId = tag.id
+                        } else {
+                            disambiguationId = nil
+                        }
+                    }, label: {
+                        HStack {
+                            Image(systemName: disambiguationId == tag.id ? "checkmark.circle" : "circle").font(.title)
+                        }
+                    })
+                    TagView(tag: tag, fullWidth: true)
+                    .swipeActions {
                         Button(role: .destructive, action: {
                             if let index = parentTags.firstIndex(where: {$0.id == tag.id}) {
                                 parentTags.remove(at: index)
                             }
                         }) {
-                            Image(systemName: "minus")
-                                .frame(minHeight: 0, maxHeight: .infinity)
+                            Label("Delete", systemImage: "trash")
                         }
-                        .tint(.red)
-                        .buttonStyle(.bordered)
                     }
                 }
-                Button(action: {
-                    showTagParentSelector = true
-                }) {
-                    Label("Add Parent Tag", systemImage: "plus")
-                        .frame(minWidth: 0, maxWidth: .infinity)
+                .alignmentGuide(.listRowSeparatorLeading) { viewDimensions in
+                  return 0
                 }
-                .tint(.blue)
-                .buttonStyle(.bordered)
-                .sheet(isPresented: $showTagParentSelector) {
-                    NavigationView {
-                        ScrollView {
-                            VStack {
-                                ForEach(tags) { tag in
-                                    Button(action: {
-                                        parentTags.filter({$0.id == tagId}).count == 0 ? parentTags.append(tag) : ()
-                                        showTagParentSelector = false
-                                    }) {
-                                        TagView(tag: tag, fullWidth: true)
-                                    }
-                                }
-                            }
-                            .padding(16)
-                        }
-                        .navigationTitle("Tags")
-                        .toolbar {
-                            ToolbarItem(placement: .navigationBarLeading){
+            }
+            Button(action: {
+                showTagParentSelector = true
+            }) {
+                Label("Add Parent Tag", systemImage: "plus")
+            }
+            .sheet(isPresented: $showTagParentSelector) {
+                NavigationView {
+                    ScrollView {
+                        VStack {
+                            ForEach(tags) { tag in
                                 Button(action: {
+                                    parentTags.filter({$0.id == tagId}).count == 0 ? parentTags.append(tag) : ()
                                     showTagParentSelector = false
                                 }) {
-                                    Image(systemName: "chevron.backward")
+                                    TagView(tag: tag, fullWidth: true)
                                 }
+                            }
+                        }
+                        .padding(16)
+                    }
+                    .navigationTitle("Tags")
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarLeading){
+                            Button(action: {
+                                showTagParentSelector = false
+                            }) {
+                                Image(systemName: "chevron.backward")
                             }
                         }
                     }
                 }
             }
         }
+        .listStyle(.plain)
     }
 }
