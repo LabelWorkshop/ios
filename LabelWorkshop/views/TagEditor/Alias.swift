c4727398e9187cd17f9484d473eed312d6332b05
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2026-07-19 19:21:31 -0400

    refactor(ui): move aliases into list

--- LabelWorkshop/views/TagEditor/Alias.swift
+++ LabelWorkshop/views/TagEditor/Alias.swift
@@ -5,33 +5,26 @@ struct TagEditorAlias: View {
     var tagId: Int
     
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach($aliases){ $alias in
-                    HStack {
-                        TextField("Alias", text: $alias.name)
+        List {
+            ForEach($aliases){ $alias in
+                TextField("Alias", text: $alias.name)
+                    .swipeActions {
                         Button(role: .destructive, action: {
                             if let index = aliases.firstIndex(where: {$0.id == alias.id}) {
                                 aliases.remove(at: index)
                             }
                         }) {
-                            Image(systemName: "minus")
-                                .frame(minHeight: 0, maxHeight: .infinity)
+                            Label("Delete", systemImage: "trash")
                         }
-                        .tint(.red)
-                        .buttonStyle(.bordered)
                     }
-                }
-                Button(action: {
-                    aliases.append(TagAlias(id: Int.random(in: (-9999)..<(-1)), name: "", tagId: tagId))
-                }) {
-                    Label("Add Alias", systemImage: "plus")
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                }
-                .tint(.blue)
-                .buttonStyle(.bordered)
+            }
+            Button(action: {
+                aliases.append(TagAlias(id: Int.random(in: (-9999)..<(-1)), name: "", tagId: tagId))
+            }) {
+                Label("Add Alias", systemImage: "plus")
             }
         }
+        .listStyle(.plain)
     }
 }
 
