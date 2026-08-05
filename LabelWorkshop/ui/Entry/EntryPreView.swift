20b0744528d41c8518be00df62cd847d9ef7cacd
purpletennisball <212221402+purpletennisball@users.noreply.github.com>
2026-08-05 15:46:46 -0400

    chore: move ExtensionTypes into new file

--- LabelWorkshop/ui/Entry/EntryPreView.swift
+++ LabelWorkshop/ui/Entry/EntryPreView.swift
@@ -218,44 +218,6 @@ actor ThumbnailLoader {
     }
 }
 
-enum ExtensionTypes {
-    var supportsStillFrame: Bool {
-        switch self {
-        case .AnimatedImage, .Image, .Video:
-            true
-        default :
-            false
-        }
-    }
-    
-    var systemImage: String {
-        switch self {
-        case .Audio:
-            "waveform"
-        case .Image:
-            "photo"
-        case .Video:
-            "movieclapper"
-        case .Archive:
-            "zipper.page"
-        case .PlainText:
-            "text.document"
-        case .AnimatedImage:
-            "square.3.layers.3d.down.forward"
-        default:
-            "questionmark.folder"
-        }
-    }
-    
-    case Image
-    case Video
-    case AnimatedImage
-    case Audio
-    case Archive
-    case PlainText
-    case Unknown
-}
-
 struct EntryPreView: View {
     public var entry: Entry
     public var square: Bool = false
